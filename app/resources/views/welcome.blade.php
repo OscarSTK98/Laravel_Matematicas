5a13e4b7fc76f88ea861e07b4700cb61cd38b262
Oscar David <44876247+OscarSTK98@users.noreply.github.com>
2020-03-26 01:51:14 -0600

    Implementación Front
    Successful

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -1,100 +1,168 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>Laravel</title>
+        <title>Collapsible sidebar using Bootstrap 3</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+         <!-- Bootstrap CSS CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Our Custom CSS -->
+        <link rel="stylesheet" href="../www/css/general.css">
+        <link rel="stylesheet" href="../www/css/owlcarousel/owl.carousel.min.css" />
+        <link rel="stylesheet" href="../www/css/owlcarousel/owl.theme.default.css" />
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+    <body class="bg">
+        <div class="wrapper">
+            <!-- Sidebar Holder -->
+            <nav id="sidebar">
+                <div class="sidebar-header">
+                    <h3 class="text-center">
+                        <img class="logo__big" src="../www/images/CECYT3.png">
+                    </h3>
+                    <strong class="text-center">
+                        <img class="logo__mini" src="../www/images/CECYT3.png">
+                    </strong>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <ul class="list-unstyled components">
+                    <li class="">
+                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-education"></i>
+                            Alumnos
+                        </a>
+                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                            <li><a href="#">Material</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">
+                            <i class="glyphicon glyphicon-apple"></i>
+                            Profesores
+                        </a>
+                        <ul class="collapse list-unstyled" id="pageSubmenu">
+                            <li><a href="#">Inicio sesión</a></li>
+                        </ul>
+                    </li>
+                    <!--<li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-link"></i>
+                            Portfolio
+                        </a>
+                    </li>-->
+                </ul>
+
+                <!--<ul class="list-unstyled CTAs">
+                    <li><a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a></li>
+                    <li><a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a></li>
+                </ul>-->
+            </nav>
+
+            <!-- Page Content Holder -->
+            <div id="content">
+
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+
+                        <div class="navbar-header">
+                            <span id="sidebarCollapse" class="sidebarButton">
+                                <i class="glyphicon glyphicon-menu-hamburger"></i>
+                            </span>
+                        </div>
+
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <!--<ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Page</a></li>
+                                <li><a href="#">Page</a></li>
+                                <li><a href="#">Page</a></li>
+                                <li><a href="#">Page</a></li>
+                            </ul>-->
+                        </div>
+                    </div>
+                </nav>
+
+                <h3 class="text-center">Centro de Estudios Cientificos y Tecnológicos #3 "Estanislao Ramírez Ruíz"</h3>
+                <h3 class="text-center">¡Bienvenid@ al sitio oficial de la academía de matemáticas!</h3>
+
+                <div class="text-center">
+                    <img class="img__main" src="../www/images/CECYT3.png">
+                </div>
+
+                <h4 class="text-center"><i>"En las matemáticas es donde el espiritu encuentra los elementos que más ansía: la continuidad y la perseverancia"</i></h4>
+                <h4 class="text-center fontWeight">Anatole France</h4>
+
+                <div class="container__carousel">
+                    <div class="owl-carousel owl-theme">
+                        <div class="image_container_carousel">
+                            <img class="img__main" src="../www/images/bg.jpg" />
+                            <div class="bottom-right-carousel background-image-carousel">INFO SOBRE LA IMAGEN</div>
+                        </div>
+                        <div class="image_container_carousel">
+                            <img class="img__main" src="../www/images/bg1.jpg" />
+                            <div class="bottom-right-carousel background-image-carousel">INFO SOBRE LA IMAGEN</div>
+                        </div>
+                        <div class="image_container_carousel">
+                            <img class="img__main" src="../www/images/bg2.jpg" />
+                            <div class="bottom-right-carousel background-image-carousel">INFO SOBRE LA IMAGEN</div>
+                        </div>
+                        <div class="image_container_carousel">
+                            <img class="img__main" src="../www/images/bg4.jpg" />
+                            <div class="bottom-right-carousel background-image-carousel">INFO SOBRE LA IMAGEN</div>
+                        </div>
+                        <div class="image_container_carousel">
+                            <img class="img__main" src="../www/images/bg5.jpg" />
+                            <div class="bottom-right-carousel background-image-carousel">INFO SOBRE LA IMAGEN</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="line"></div>
+                <div class="card-footer text-muted footer__main">
+                    <div class="row">
+                        <div class="column mr_web" id="footer1">
+                            <div class="mb_ fontWeight">
+                                Instituto Politécnico Nacional
+                            </div>
+                            <div class="text_j">
+                                Academia de Matemáticas Turno Matutino del Centro de Estudios Científicos y Tecnológicos #3 "Estanislao Ramírez Ruíz"
+                            </div>
+                        </div>
+                        <div class="column" id="footer1">
+                            <div class="mb_ mt_app fontWeight">
+                                Contacto
+                            </div>
+                            <div class="text_j">
+                                Av. Carlos Hank González s/n, Col, Valle de Ecatepec, 55119 Ecatepec de Morelos, Méx.
+                            </div>
+                            <div class="text_j">
+                                Tel. 55 5624 2000
+                            </div>
+                            <!-- <div class="text_j">
+                                Página Oficial CECyT 3
+                            </div>
+                            <div class="text_j">
+                                Página Oficial IPN
+                            </div> -->
+                        </div>
+                    </div>    
+                    <div class="footer__bottom footer__textAlign">
+                        © <span class="currentYear"></span> Copyright: <span class="fontWeight">Sistema creado por alumnos de la Escuela Superior de Cómputo.</span>
+                    </div>
                 </div>
             </div>
         </div>
+
+
+
+
+
+        <!-- jQuery CDN -->
+         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+         <script src="../www/css/owlcarousel/owl.carousel.min.js"></script>
+         <!-- Bootstrap Js CDN -->
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="../www/js/index.js"></script>
     </body>
 </html>
